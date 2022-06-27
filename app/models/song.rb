75047a1af40ad6a0f91b95240a1cef2de8e1a15b
Luke Baltay <luke.baltay@yale.edu>
2022-06-27 15:12:35 -0400

    commit tree b668fbcd2245a9c9518dc9d23173c487d32e7530

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,14 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artists
+  belongs_to :genres
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
 end
